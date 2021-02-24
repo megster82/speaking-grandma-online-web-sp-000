2dcabb998980c4c025af576cbd4cbf0a336f4e4e
meaghan jennings <meaghanljennings@gmail.com>
2021-02-24 21:13:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,2 +1,10 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end 
